041da07e13d8432049832b2b0220d819dd3c550d
SaJaD. <164061116+iqtnt@users.noreply.github.com>
2025-08-27 13:13:28 +0300

    Update upf.py

--- upf.py
+++ upf.py
@@ -9,6 +9,7 @@ def value_type(v):
         return 'str'
 
 seen = set()
+filter_params = "-p" in sys.argv
 
 for line in sys.stdin:
     url = line.strip()
@@ -16,9 +17,12 @@ for line in sys.stdin:
         continue
 
     parsed = urlparse(url)
+    params = parse_qsl(parsed.query)
 
-    params_with_type = tuple([(p[0], value_type(p[1])) for p in parse_qsl(parsed.query)])
+    if filter_params and not params:
+        continue
 
+    params_with_type = tuple([(p[0], value_type(p[1])) for p in params])
     key = (parsed.netloc, parsed.path, params_with_type)
 
     if key not in seen:
